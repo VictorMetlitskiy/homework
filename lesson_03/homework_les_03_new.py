b3e9335ca024badba0b3924a9348df5a79431df8
VictorMetlitskiy <victormetlitskiy@gmail.com>
2022-02-06 10:32:23 +0200

    change homework_les_03_new.py

--- lesson_03/homework_les_03_new.py
+++ lesson_03/homework_les_03_new.py
@@ -2,7 +2,7 @@
 weight = 82
 height = 1.74
 IBM = weight / (height * height)
-print(('Your BMI is normal: False', 'Your BMI is normal: True')[18.5 < IBM < 25])
+print(f'Your BMI is normal: {18.5 < IBM < 25}')
 
 # Task 2.
 valid_number = input()
@@ -10,4 +10,4 @@ check1 = len(valid_number) == 12
 check2 = valid_number.isdigit()
 check3 = valid_number.startswith('380')
 total = check1 + check2 + check3
-print(('Invalid', 'Invalid', 'Invalid', 'Valid')[total], 'number')
+print(f'This number is valid: {total == 3}')
