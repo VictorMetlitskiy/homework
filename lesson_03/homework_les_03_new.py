08e7c2fafcb1e5991851895594ad81f0d14c84af
VictorMetlitskiy <victormetlitskiy@gmail.com>
2022-02-06 21:05:56 +0200

    change homework_les_03_new.py (variable's name)

--- lesson_03/homework_les_03_new.py
+++ lesson_03/homework_les_03_new.py
@@ -5,9 +5,9 @@ IBM = weight / (height * height)
 print(f'Your BMI is normal: {18.5 < IBM < 25}')
 
 # Task 2.
-valid_number = input()
-check1 = len(valid_number) == 12
-check2 = valid_number.isdigit()
-check3 = valid_number.startswith('380')
+phone_number = input()
+check1 = len(phone_number) == 12
+check2 = phone_number.isdigit()
+check3 = phone_number.startswith('380')
 total = check1 + check2 + check3
 print(f'This number is valid: {total == 3}')
