1c1435917f7f02f77f1a3d10d8d17cb7fd6ed44b
VictorMetlitskiy <victormetlitskiy@gmail.com>
2022-02-07 18:47:21 +0200

    change homework_les_02.py (variable's name)

--- lesson_02/homework_les_02.py
+++ lesson_02/homework_les_02.py
@@ -7,19 +7,19 @@ def draw_box():
 
 # Use function print wiht different argements
 numbers = 1, 2, 3, 4, 5, 6, 7
-lst = [num for num in range(10)]
-s = 'Happy_Pythoning!'
+lst_numbers = [num for num in range(10)]
+text = 'Happy_Pythoning!'
 
 if __name__ == '__main__':
 # Output: Happy_Pythoning!
-    print(s)
+    print(text)
 # Output: Happy_Pythoning!*Happy_Pythoning!Happy_Pythoning!Happy_Pythoning!
-    print(s, s*3, sep='*', end='\n\n')
+    print(text, text*3, sep='*', end='\n\n')
 # Output: (1, 2, 3, 4, 5, 6, 7)
     print(numbers)
 # Output: 0 1 2 3 4 5 6 7 8 9
-    print(*lst)
+    print(*lst_numbers)
 # Output: H-a-p-p-y-_-P-y-t-h-o-n-i-n-g-!
-    print('-'.join(s))
+    print('-'.join(text))
     print()
     draw_box()
