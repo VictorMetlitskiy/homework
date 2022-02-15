273418500f14a656200ffa6b557c1b89c220bc05
VictorMetlitskiy <victormetlitskiy@gmail.com>
2022-02-15 14:11:30 +0200

    creation task_1_les_07.py

---
+++ lesson_07/task_1_les_07.py
@@ -0,0 +1,34 @@
+# Task 1.
+# Define function for deleting punctuation from string.
+
+
+def del_punctuation(string):
+    if ',' in string:
+        string = string.replace(',', '')
+    if '.' in string:
+        string = string.replace('.', '')
+    if ';' in string:
+        string = string.replace(';', '')
+    if ':' in string:
+        string = string.replace(':', '')
+    if '?' in string:
+        string = string.replace('?', '')
+    if '!' in string:
+        string = string.replace('!', '')
+    return string
+
+
+text = input('Write, please, any sentence for future dictionary: ')
+
+text = del_punctuation(text)
+lst_word = text.split()
+dict_word = {}
+for word in lst_word:
+    dict_value = lst_word.count(word)
+    dict_word[word] = dict_value
+
+# Creation the dict using comprehension.
+dict_word2 = {word: lst_word.count(word) for word in lst_word}
+
+print(dict_word)
+print(dict_word2)
