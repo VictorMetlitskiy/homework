d20d9dd32aca81eab8a0820b8ab41b043b0a21ef
VictorMetlitskiy <victormetlitskiy@gmail.com>
2022-02-15 14:16:34 +0200

    creation task_2_les_07.py

---
+++ lesson_07/task_3_les_07.py
@@ -0,0 +1,4 @@
+# Task 3. Use a list comprehension to make a list containing tuples (i, j),
+# where `i` goes from 1 to 10 and `j` is corresponding to `i` squared.
+lst_tuples = [(num, num**2) for num in range(1, 11)]
+print(lst_tuples)
