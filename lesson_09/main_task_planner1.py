aecc33bbb03a73614395322ad44a96525a1d47c7
VictorMetlitskiy <victormetlitskiy@gmail.com>
2022-02-22 12:37:22 +0200

    Change main_task_planner1.py

--- lesson_09/main_task_planner1.py
+++ lesson_09/main_task_planner1.py
@@ -1,8 +1,9 @@
-from task_planner_main_func1 import read_file
+import json
+import os.path
+
 from task_planner_main_func1 import print_cli
 from task_planner_main_func1 import select_function
 from task_planner_main_func1 import execute_func_0
-from task_planner_main_func1 import create_task
 from task_planner_main_func1 import review_tasks_list
 from task_planner_main_func1 import review_task_detail
 from task_planner_main_func1 import edit_task
@@ -13,23 +14,43 @@ from task_planner_main_func1 import find_overdue_task
 from task_planner_main_func1 import save_cur_tasks
 
 from task_planner_ad_func1 import get_id_for_edit
+from task_planner_ad_func1 import get_id
+from task_planner_ad_func1 import get_title
+from task_planner_ad_func1 import get_description
+from task_planner_ad_func1 import get_priority
+from task_planner_ad_func1 import get_due_date
+from task_planner_ad_func1 import change_date_to_str
+from task_planner_ad_func1 import change_str_to_date
 
 lst_command_line = ['Command Line Interface', 'Finish work', 'Create new task', 'Review list of tasks',
                     'Review detail of task', 'Edit a task', 'Delete a task', 'Find by name', 'Sort by priority',
                     'Find overdue task', 'Save the current tasks'
                     ]
 
+lst_tasks = []
 lst_keys = ['id', 'title', 'description', 'priority', 'status', 'due_date']
-lst_tasks = read_file()
-
+if os.path.exists('./data/lst_tasks.json'):
+    with open('./data/lst_tasks.json') as file_object:
+        lst_tasks = json.load(file_object)
+        lst_tasks = change_str_to_date(lst_tasks)
 while True:
     print_cli(lst_command_line)
     number_function = select_function()
     if number_function == 0:
-        execute_func_0(lst_tasks)
+        lst_tasks = change_date_to_str(lst_tasks)
+        with open('./data/lst_tasks.json', 'w') as file_object:
+            json.dump(lst_tasks, file_object)
+        execute_func_0()
         break
     if number_function == 1:
-        dict_task = create_task(lst_tasks, lst_keys)
+        task_id = get_id(lst_tasks)
+        task_title = get_title()
+        task_description = get_description()
+        task_priority = get_priority()
+        task_status = 'pending'
+        task_due_date = get_due_date()
+        lst_values = [task_id, task_title, task_description, task_priority, task_status, task_due_date]
+        dict_task = {lst_keys[i]: lst_values[i] for i in range(len(lst_keys))}
         lst_tasks.append(dict_task)
         print('Task has been created successfully.')
     if number_function == 2:
